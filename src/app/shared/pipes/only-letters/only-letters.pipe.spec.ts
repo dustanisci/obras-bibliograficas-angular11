b5ea829620cf911112aae8fd9c4f4c864fb12126
Eduardo Stanisci <dustanisci@outlook.com>
2021-01-25 22:53:31 -0300

    dist

--- src/app/shared/pipes/only-letters/only-letters.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { OnlyLettersPipe } from './only-letters.pipe';
-
-describe('OnlyLettersPipe', () => {
-  it('create an instance', () => {
-    const pipe = new OnlyLettersPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
